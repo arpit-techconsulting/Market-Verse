b140d6d1674f1e8c8a72789b76b7404618e52b25
arpit5492 <Arpit.Mallick@techconsulting.tech>
2024-10-03 00:05:55 -0400

    Api call is working fine

--- MarketVerse/MarketVerseApp.swift
+++ MarketVerse/MarketVerseApp.swift
@@ -13,7 +13,7 @@ struct MarketVerseApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Home()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
