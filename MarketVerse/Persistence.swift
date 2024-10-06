c516872d20ae722072c6ae1c37123119131b80ff
arpit5492 <Arpit.Mallick@techconsulting.tech>
2024-10-05 20:14:19 -0400

    Saving data to Core data and removed duplicate entries

--- MarketVerse/Persistence.swift
+++ MarketVerse/Persistence.swift
@@ -15,19 +15,19 @@ struct PersistenceController {
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "MarketVerse")
 
-        // Use an in-memory store if needed, otherwise use the default persistent store
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
 
-//        container.loadPersistentStores { _, error in
-//            if let error = error as NSError? {
-//                // Handle the error properly in production
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        }
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error loading persistent store: \(error), \(error.userInfo)")
+            } else {
+                print("Persistent store loaded successfully")
+            }
+        }
 
-        // Enable automatic merging from parent context
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
+
--- MarketVerse/Views/Home.swift
+++ MarketVerse/Views/Home.swift
@@ -15,6 +15,8 @@ struct Home: View {
         Text("Hello, World!")
         
             .onAppear {
+                let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+                print(paths[0])
                 Task {
                     await homeViewModel.fetchApiData()
                 }
