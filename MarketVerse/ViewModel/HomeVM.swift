191500f0d8fda98d1e29b76f18bd813cef9b3cc6
arpit5492 <Arpit.Mallick@techconsulting.tech>
2024-10-06 02:39:10 -0400

    Handled multiple tab switching error

--- MarketVerse/ViewModel/HomeVM.swift
+++ MarketVerse/ViewModel/HomeVM.swift
@@ -12,10 +12,20 @@ struct HomeVM {
     private let coreDataManager = CoreDataManager() // there are two dependencies in one structure, work on removing the dependencies (not a good practice)
     
     func fetchApiData() async {
+        // Check if data is already saved in Core Data
+        let isDataSaved = UserDefaults.standard.bool(forKey: "isDataSaved")
+        guard !isDataSaved else {
+            print("Data is already saved in Core Data. Skipping Api Call")
+            return
+        }
+        
         do {
             let apiData: Model = try await networkManager.apiCall(apiUrl: "https://dummyjson.com/products?limit=0&skip=0")
 //            print(apiData)
             await coreDataManager.saveToCoreData(products: apiData.products)
+            
+            // Set flag for storing data in Core Data
+            UserDefaults.standard.set(true, forKey: "isDataSaved")
         } catch {
             print("Error in fetching data from Api: \(error.localizedDescription)")
         }
--- MarketVerse/Views/Home.swift
+++ MarketVerse/Views/Home.swift
@@ -7,20 +7,41 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct Home: View {
     
     private let homeViewModel = HomeVM()
     
     var body: some View {
-        Text("Hello, World!")
-        
-            .onAppear {
-                let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
-                print(paths[0])
-                Task {
-                    await homeViewModel.fetchApiData()
-                }
+        TabView {
+            // Home Tab
+            VStack {
+                Text("Home View")
+                    .onAppear {
+                        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+                        print(paths[0])
+                        Task {
+                            await homeViewModel.fetchApiData()
+                        }
+                    }
             }
+            .tabItem {
+                Label("Home", systemImage: "house.fill")
+            }
+            
+            // Favorites Tab
+            Text("Favorites View")
+                .tabItem {
+                    Label("Favorites", systemImage: "star.fill")
+                }
+            
+            // Account Tab
+            Text("Account View")
+                .tabItem {
+                    Label("Account", systemImage: "person.fill")
+                }
+        }
     }
 }
 
